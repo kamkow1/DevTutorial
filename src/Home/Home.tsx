ca5c32f59945c70fe6489a3a246e50c36c9b3ed1
kamkow1 <ikamileq@gmail.com>
2022-02-11 18:08:36 -0800

    changes to home page text

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -9,8 +9,15 @@ function Home() {
                 W pasku po lewej wybierz sekcję, do której chcesz przejść
             </p>
 
-            <div className="container">
-                
+            <div className="main-content-wrapper">
+                <div className="main-content">
+                    <section>
+                        <h5>Co oferuje DevTutorial?</h5>
+                        <p>
+                            
+                        </p>
+                    </section>
+                </div>
             </div>
         </div>
     );
